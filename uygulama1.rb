262086fb3e4b516a78267fcc948bfd901cd122d1
tugcerrn <t_ereren92@hotmail.com>
2015-09-01 18:05:39 +0300

    uygulama1 eklendi

---
+++ uygulama1.rb
@@ -0,0 +1,21 @@
+print "enter the first number,please="
+num1=gets
+print "enter the second number,please="
+num2=gets
+def add (num1,num2)
+  num1+num2
+end
+def subtract(num1,num2)
+  num1-num2
+end
+def multiply(num1,num2)
+  num1*num2
+end
+def divide(num1,num2)
+  num1/num2
+end
+puts add(num1,num2)
+puts subtract(num1,num2)
+puts multiply(num1,num2)
+puts divide(num1,num2)
+
---
+++ uygulama4.rb
@@ -0,0 +1,2 @@
+puts "tugce".upcase
+
