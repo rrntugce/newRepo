dd27ccb106f70750591af12e75fe7a8b17effa2d
tugcerrn <t_ereren92@hotmail.com>
2015-09-02 15:53:53 +0300

    uygulama4 eklendi.

--- uygulama3.rb
+++ uygulama3.rb
@@ -1,1 +1,14 @@
-puts 3**4
+print "cumlenizi yaziniz=>"
+sentence=gets.chomp
+sentence.downcase!
+print "aranilacak kelimeyi giriniz=>"
+word=gets.chomp
+word.downcase!
+counter=0
+if sentence.include?("#{word}")
+  counter +=1
+  puts "aranilan kelime cumlede mevcut ve sayisi #{counter}"
+else
+  puts "aranilan kelime mevcut değil"
+end
+
--- uygulama4.rb
+++ uygulama4.rb
@@ -1,2 +1,13 @@
-puts "tugce".upcase
+print "uzunlugu bulunacak cumleyi giriniz=>"
+metin=gets.chomp
+uzunluk=metin.length
+print "girdiginiz cumlenin uzunlugu #{uzunluk}"
+
+
+
+
+
+
+
+
 
