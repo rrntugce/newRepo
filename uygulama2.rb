1b56253b7de97aaff646191e209655e29774dd4d
tugcerrn <t_ereren92@hotmail.com>
2015-09-02 10:56:32 +0300

    Uygulama2 Palindrom eklendi.

--- uygulama2.rb
+++ uygulama2.rb
@@ -1,1 +1,13 @@
-puts 3+3 
+
+print "you should write a string => "
+string = gets.chomp
+string.downcase
+if string.include?(" ")
+  string.gsub!(//, "")
+end
+if string== string.reverse
+  puts "is a palindrome"
+else
+  puts  "is not a palindrome"
+
+end
